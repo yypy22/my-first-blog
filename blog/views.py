0b8f570469859c5d0d8a04438501c7eb3a17ca34
Eiji Miyamoto <motopon57@gmail.com>
2022-03-30 21:16:06 +0000

    numnum

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.utils import timezone
 # Create your views here.
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
-    
+def post_list(request):]
+    Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
     
